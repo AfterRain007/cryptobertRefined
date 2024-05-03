723a2aee51daa400a7774c78d34d6ded98aaf862
AfterRain007 <afterrain007@gmail.com>
2024-05-03 14:48:57 +0700

    Added augmented Data

--- util/check.py
+++ util/check.py
@@ -1,1 +1,22 @@
 import os
+
+langCodeGT = ['it', 'fr', 'sv', 'da', 'pt',
+              'id', 'pl', 'hr', 'bg', 'fi',
+              'no', 'ru', 'es', 'nl', 'af',
+              'de', 'sk', 'cs', 'lv', 'sq']
+
+langCodeHNLP = ['zh', 'es', 'ru', 'jap', 
+                'de', 'fr', 'it', 'id']
+
+def check_files():
+    for lang in langCodeGT:
+        if not os.path.exists(f"./augmented_data/dfTrain-{lang}GT.csv"):
+            return False
+        
+    for lang in langCodeHNLP:
+        if not os.path.exists(f"./augmented_data/dfTrain-{lang}HNLP.csv"):
+            return False
+    
+    return True
+
+print(check_files())
